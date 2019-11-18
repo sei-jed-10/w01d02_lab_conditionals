0013d794b8cd626c71d1bcc313c1f5f7781c7fbe
KOSaber <k.o.saber@hotmail.com>
2019-11-18 16:47:50 +0300

    its done

--- js/main.js
+++ js/main.js
@@ -12,9 +12,21 @@ The following numbers represent each input
  If Rock and paper, Rock wins
  If Rock and Scissor, Rock wins
  If Paper and Scissor, Scissor wins
- If Paper and Rock, Rock wins
 
  Start by writing a Pseudocode to ensure you have your logic right before writing anycode.
 */
 
 let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let computerNum = Math.floor(Math.random() * 3) + 1;
+console.log(Math.random())
+console.log("computer : " + Math.floor(Math.random() * 3) + 1)
+console.log("user : " + userInput )
+
+if(userInput == computerNum)
+console.log('its equals')
+else if((userInput == 1 && computerNum == 3) || (userInput == 3 && computerNum == 1 ))
+console.log('paper is wins')
+else if((userInput == 3 && computerNum == 2) || (userInput == 2 && computerNum == 3 ))
+console.log('Rock is wins')
+else if((userInput == 2 && computerNum == 1) || (userInput == 1 && computerNum == 2 ))
+console.log('Scissors is wins')
