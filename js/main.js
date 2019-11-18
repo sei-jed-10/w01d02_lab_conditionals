73f4af95daba2967a41f198966751edb1f47badc
Bedo-11 <ibedo3@gmail.com>
2019-11-18 16:26:32 +0300

    The game is working!

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ The following numbers represent each input
  3 - Rock
  i.e. 1 means paper
 
- If Rock and paper, Rock wins
+ If Rock and paper, Paper wins
  If Rock and Scissor, Rock wins
  If Paper and Scissor, Scissor wins
  If Paper and Rock, Rock wins
@@ -17,4 +17,24 @@ The following numbers represent each input
  Start by writing a Pseudocode to ensure you have your logic right before writing anycode.
 */
 
-let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock');
+
+let copmuterNum = Math.floor(Math.random() * 3) + 1;
+let result = "";
+if (userInput == 3 && copmuterNum == 1) {
+    result += "Paper wins";
+} else if (userInput == 3 && copmuterNum == 2) {
+    result += "Rock wins";
+} else if (userInput == 1 && copmuterNum == 2) {
+    result += "Scissor wins";
+} else if (userInput == 1 && copmuterNum == 3) {
+    result += "Paper wins";
+} else if (userInput == 2 && copmuterNum == 3) {
+    result += "Rock wins";
+} else if (userInput == 2 && copmuterNum == 1) {
+    result += "Scissor wins";
+} else if (userInput == copmuterNum) {
+    result += "Tie!";
+}
+
+console.log(result);
