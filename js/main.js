3a21ba29c87a2c0e03f84d045d15a74136f6e170
guru-schnecke <eiweala@live.com>
2019-11-18 14:11:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+ Lets make a game of Rock paper scissor.
+ +------+
+  Rules
+ +------+
+The following numbers represent each input
+ 1 - Paper
+ 2 - Scissors
+ 3 - Rock
+ i.e. 1 means paper
+
+ If Rock and paper, Rock wins
+ If Rock and Scissor, Rock wins
+ If Paper and Scissor, Scissor wins
+ If Paper and Rock, Rock wins
+
+ Start by writing a Pseudocode to ensure you have your logic right before writing anycode.
+*/
