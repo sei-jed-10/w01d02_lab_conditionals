45d33e6a84bc751f3215f5019689910032e4bb0b
Abdullah <Abdullah@Ashwaqs-MacBook-Pro.local>
2019-11-18 16:51:54 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ The following numbers represent each input
  3 - Rock
  i.e. 1 means paper
 
- If Rock and paper, Rock wins
+ If Rock and paper, Paper wins
  If Rock and Scissor, Rock wins
  If Paper and Scissor, Scissor wins
  If Paper and Rock, Rock wins
@@ -18,3 +18,20 @@ The following numbers represent each input
 */
 
 let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let computerNum = (Math.floor(Math.random() * 3) + 1)
+
+if (computerNum == userInput)
+{console.log("Tie")}
+
+if ((computerNum == 1 && userInput == 2) || (computerNum == 2 && userInput == 1) || (computerNum == 3 && userInput == 2))
+{console.log("You Lose")}
+
+if (computerNum == 1 && userInput == 3) || (computerNum == 2 && userInput == 3) || (computerNum == 3 && userInput == 1))
+{console.log("You Win")}
+
+if (userInput < 1 || userInput > 3)
+{console.log("Wrong input")}
+
+
+
+
