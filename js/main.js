46aacdcfd887f92126a6bfeb5588502baed2a447
Fahad Alturkistani <fahad.alturkistani@gmail.com>
2019-11-18 16:41:59 +0300

    added game logic

--- js/main.js
+++ js/main.js
@@ -17,4 +17,46 @@ The following numbers represent each input
  Start by writing a Pseudocode to ensure you have your logic right before writing anycode.
 */
 
-let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let computerNum = Math.floor(Math.random()*3)+1;
+let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock');
+console.log("user entered: "+userInput);
+console.log("computer entered: "+computerNum);
+if (userInput == computerNum){
+    console.log("It's a tie!");
+    alert("It's a tie!");
+}
+if ( (userInput == 3 && computerNum == 1) || (userInput == 1 && computerNum == 3) ){
+    console.log("Paper Wins!");
+    alert("Paper Wins!");
+    if (userInput == 1) {
+        alert("User Won");
+    }
+    else 
+        alert("Computer Won");
+}
+if ( (userInput == 2 && computerNum == 1) || (userInput == 1 && computerNum == 2) ){
+    console.log("Scissors Wins!");
+    alert("Scissors Wins!");
+    if (userInput == 2) {
+        alert("User Won");
+    }
+    else 
+        alert("Computer Won");
+}
+if ( (userInput == 3 && computerNum == 2) || (userInput == 2 && computerNum == 3) ){
+    console.log("Rock Wins!");
+    alert("Rock Wins!");
+    if (userInput == 3) {
+        alert("User Won");
+    }
+    else 
+        alert("Computer Won");
+}
+if (userInput > 3 || userInput < 0){
+    console.log('Invalid input!!!');
+    alert("Invalid input!!!");
+}
+
+
+
+
