5df3dc0b381613df13cb77d56055b145ef1f89e3
Rooz <razan@gmail.com>
2019-11-18 17:26:07 +0300

    fixing code

--- js/main.js
+++ js/main.js
@@ -18,3 +18,33 @@ The following numbers represent each input
 */
 
 let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let computerNum = Math.floor(Math.random() * 3) + 1;
+console.log("computerNum")
+console.log("userInput") 
+var Peper = 1
+var Scissor = 2
+var Rock = 3
+
+Console.log (Math.floor(Math.random() * 3) + 1)
+
+
+if (userInput == computerNum) 
+{
+    console.log("tie")
+}
+
+ if(userInput == 1 && computerNum == 2) || (userInput ==2 && computerNum == 1) 
+ {
+    console.log("Scissor wins")
+}
+
+if (userInput == 1 && computerNum ==3) || (userInput ==3 && computerNum == 1)
+{
+    console.log("Paper wins")
+}
+
+
+if (userInput == 2 && computerNum == 3) || (userInput == 3 && computerNum == 2)
+{
+    console.log("rock wins")
+} 
