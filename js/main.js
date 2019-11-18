488af08fa7a5fc3027fdfcf515653087b1449826
Mohammed <moohyacc@gmail.com>
2019-11-18 16:41:53 +0300

    Game is ready!

--- js/main.js
+++ js/main.js
@@ -18,3 +18,27 @@ The following numbers represent each input
 */
 
 let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+
+let rand = Math.floor(Math.random() *3) +1;
+
+console.log("Computer played:\t" + rand)
+console.log("You played:   \t\t" + userInput)
+
+if (userInput != rand) {
+    
+    if ((rand == 3 || userInput == 3) && (rand == 1 || userInput == 1)) {
+        console.log("Paper wins")
+    }
+
+    if ((rand == 3 || userInput == 3) && (rand == 2 || userInput == 2)) {
+        console.log("Rock wins")
+    }
+
+    if ((rand == 1 || userInput == 1) && (rand == 2 || userInput == 2)) {
+        console.log("Scissor wins")
+    }
+
+}else{
+    console.log("TIE!")
+}
+
