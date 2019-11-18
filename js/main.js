7a1f6aa9cc63972e88c081290dbea0847028b255
Amal337 <amal.grergi@icloud.com>
2019-11-18 16:47:47 +0300

    modified js file

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ The following numbers represent each input
 */
 
 let userInput = prompt('Enter a number: 1 - Paper, 2 - Scissor, 3 - Rock')
+let computerInput = (Math.floor(Math.random() * 3) + 1)
+
+if (userInput == computerInput){
+    prompt("it's a tie")
+ }
+if (userInput == 1 && computerInput == 2 || userInput == 2 && computerInput == 1) {
+    prompt( "Scissors wins")
+}
+if (userInput == 1 && computerInput == 3 || userInput == 3 && computerInput == 1) {
+    prompt( "paper  wins")
+}
+if (userInput == 2 && computerInput == 3 || userInput == 3 && computerInput == 2) {
+    prompt( "Scissors wins")  
+}
+
+
